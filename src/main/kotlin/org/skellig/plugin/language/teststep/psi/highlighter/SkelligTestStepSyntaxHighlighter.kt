6dff0e6fa991712cbfb9cb4f8b870ab523809c84
shendor <shendor1988@gmail.com>
2023-12-04 21:53:31 +0000

    Fixed highlighter for test step syntax

--- src/main/kotlin/org/skellig/plugin/language/teststep/psi/highlighter/SkelligTestStepSyntaxHighlighter.kt
+++ src/main/kotlin/org/skellig/plugin/language/teststep/psi/highlighter/SkelligTestStepSyntaxHighlighter.kt
@@ -1,6 +1,8 @@
 package org.skellig.plugin.language.teststep.psi.highlighter
 
 import com.intellij.lexer.Lexer
+import com.intellij.openapi.editor.DefaultLanguageHighlighterColors
+import com.intellij.openapi.editor.HighlighterColors
 import com.intellij.openapi.editor.colors.TextAttributesKey
 import com.intellij.openapi.fileTypes.SyntaxHighlighterBase
 import com.intellij.psi.tree.IElementType
@@ -9,31 +11,65 @@ import org.skellig.plugin.language.teststep.psi.SkelligTestStepTypes
 
 class SkelligTestStepSyntaxHighlighter() : SyntaxHighlighterBase() {
     companion object {
-        private val ATTRIBUTES: MutableMap<IElementType, TextAttributesKey> = HashMap()
+        val COMMENT = arrayOf(
+            TextAttributesKey.createTextAttributesKey(
+                "TEST_STEP_COMMENT",
+                DefaultLanguageHighlighterColors.DOC_COMMENT
+            )
+        )
 
-        init {
-            ATTRIBUTES[SkelligTestStepTypes.NAME] = SkelligTestStepHighlighter.KEYWORD
-            ATTRIBUTES[SkelligTestStepTypes.KEYWORDS] = SkelligTestStepHighlighter.KEYWORD
-            ATTRIBUTES[SkelligTestStepTypes.KEY_SYMBOLS] = SkelligTestStepHighlighter.TEXT
-            ATTRIBUTES[SkelligTestStepTypes.FUNCTION_NAME] = SkelligTestStepHighlighter.FUNCTION
-            ATTRIBUTES[SkelligTestStepTypes.STRING] = SkelligTestStepHighlighter.STRING
-            ATTRIBUTES[SkelligTestStepTypes.REFERENCE_KEY] = SkelligTestStepHighlighter.REFERENCE
-            ATTRIBUTES[SkelligTestStepTypes.VALUE_SYMBOLS] = SkelligTestStepHighlighter.OPERATION
-            ATTRIBUTES[SkelligTestStepTypes.LESSER_EQUAL] = SkelligTestStepHighlighter.OPERATION
-            ATTRIBUTES[SkelligTestStepTypes.EQUAL] = SkelligTestStepHighlighter.OPERATION
-            ATTRIBUTES[SkelligTestStepTypes.GREATER_EQUAL] = SkelligTestStepHighlighter.OPERATION
-            ATTRIBUTES[SkelligTestStepTypes.VALUE_ASSIGN] = SkelligTestStepHighlighter.OPERATION
-            ATTRIBUTES[SkelligTestStepTypes.NOT_EQUAL] = SkelligTestStepHighlighter.OPERATION
-            ATTRIBUTES[SkelligTestStepTypes.COMMA] = SkelligTestStepHighlighter.OPERATION
-            ATTRIBUTES[SkelligTestStepTypes.REFERENCE_BRACKET] = SkelligTestStepHighlighter.BRACKETS
-            ATTRIBUTES[SkelligTestStepTypes.OBJECT_L_BRACKET] = SkelligTestStepHighlighter.BRACKETS
-            ATTRIBUTES[SkelligTestStepTypes.OBJECT_R_BRACKET] = SkelligTestStepHighlighter.BRACKETS
-            ATTRIBUTES[SkelligTestStepTypes.ARRAY_L_BRACKET] = SkelligTestStepHighlighter.BRACKETS
-            ATTRIBUTES[SkelligTestStepTypes.ARRAY_R_BRACKET] = SkelligTestStepHighlighter.BRACKETS
-            ATTRIBUTES[SkelligTestStepTypes.FUNCTION_L_BRACKET] = SkelligTestStepHighlighter.BRACKETS
-            ATTRIBUTES[SkelligTestStepTypes.FUNCTION_R_BRACKET] = SkelligTestStepHighlighter.BRACKETS
-            ATTRIBUTES[SkelligTestStepTypes.NUMBER] = SkelligTestStepHighlighter.NUMBER
-        }
+        val KEYWORD = arrayOf(
+            TextAttributesKey.createTextAttributesKey(
+                "TEST_STEP_KEYWORD",
+                DefaultLanguageHighlighterColors.KEYWORD
+            )
+        )
+
+        val BRACKETS = arrayOf(
+            TextAttributesKey.createTextAttributesKey(
+                "TEST_STEP_BRACKETS",
+                DefaultLanguageHighlighterColors.BRACKETS
+            )
+        )
+
+        val REFERENCE = arrayOf(
+            TextAttributesKey.createTextAttributesKey(
+                "REFERENCE",
+                DefaultLanguageHighlighterColors.PARAMETER
+            )
+        )
+
+        val STRING = arrayOf(
+            TextAttributesKey.createTextAttributesKey(
+                "TEST_STEP_STRING",
+                DefaultLanguageHighlighterColors.STRING
+            )
+        )
+
+        val TEXT = arrayOf(TextAttributesKey.createTextAttributesKey("TEST_STEP_TEXT", HighlighterColors.TEXT))
+
+        val FUNCTION = arrayOf(
+            TextAttributesKey.createTextAttributesKey(
+                "FUNCTION",
+                DefaultLanguageHighlighterColors.FUNCTION_CALL
+            )
+        )
+
+        val OPERATION = arrayOf(
+            TextAttributesKey.createTextAttributesKey(
+                "OPERATION",
+                DefaultLanguageHighlighterColors.OPERATION_SIGN
+            )
+        )
+
+        val NUMBER = arrayOf(
+            TextAttributesKey.createTextAttributesKey(
+                "NUMBER",
+                DefaultLanguageHighlighterColors.NUMBER
+            )
+        )
+
+//        private val BAD_CHAR = arrayOf(TextAttributesKey.createTextAttributesKey("BAD_CHARACTER", HighlighterColors.BAD_CHARACTER))
     }
 
     override fun getHighlightingLexer(): Lexer {
@@ -41,6 +77,39 @@ class SkelligTestStepSyntaxHighlighter() : SyntaxHighlighterBase() {
     }
 
     override fun getTokenHighlights(tokenType: IElementType): Array<TextAttributesKey> {
-        return pack(ATTRIBUTES[tokenType])
+        return when (tokenType) {
+            SkelligTestStepTypes.NAME -> KEYWORD
+            SkelligTestStepTypes.VALUES_KEYWORD -> KEYWORD
+            SkelligTestStepTypes.VALIDATE -> KEYWORD
+            SkelligTestStepTypes.BODY -> KEYWORD
+            SkelligTestStepTypes.MESSAGE -> KEYWORD
+            SkelligTestStepTypes.REQUEST -> KEYWORD
+            SkelligTestStepTypes.WHERE -> KEYWORD
+            SkelligTestStepTypes.PAYLOAD -> KEYWORD
+
+            SkelligTestStepTypes.KEY_SYMBOLS -> TEXT
+
+            SkelligTestStepTypes.STRING -> STRING
+
+            SkelligTestStepTypes.VALUE_SYMBOLS -> OPERATION
+            SkelligTestStepTypes.LESSER_EQUAL -> OPERATION
+            SkelligTestStepTypes.EQUAL -> OPERATION
+            SkelligTestStepTypes.GREATER_EQUAL -> OPERATION
+            SkelligTestStepTypes.VALUE_ASSIGN -> OPERATION
+            SkelligTestStepTypes.NOT_EQUAL -> OPERATION
+            SkelligTestStepTypes.COMMA -> OPERATION
+            SkelligTestStepTypes.DOT -> OPERATION
+
+            SkelligTestStepTypes.REFERENCE_BRACKET -> BRACKETS
+            SkelligTestStepTypes.OBJECT_L_BRACKET -> BRACKETS
+            SkelligTestStepTypes.OBJECT_R_BRACKET -> BRACKETS
+            SkelligTestStepTypes.ARRAY_L_BRACKET -> BRACKETS
+            SkelligTestStepTypes.ARRAY_R_BRACKET -> BRACKETS
+            SkelligTestStepTypes.FUNCTION_L_BRACKET -> BRACKETS
+            SkelligTestStepTypes.FUNCTION_R_BRACKET -> BRACKETS
+            SkelligTestStepTypes.INT -> NUMBER
+            SkelligTestStepTypes.FLOAT -> NUMBER
+            else -> emptyArray()
+        }
     }
 }
