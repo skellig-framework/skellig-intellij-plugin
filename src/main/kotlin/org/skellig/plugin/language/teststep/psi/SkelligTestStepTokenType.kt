d662d8b0e0ba2b373ac469f7cfff071cfd8217a6
shendor <shendor1988@gmail.com>
2023-12-14 12:19:54 +0000

    Added more descriptive elements of the parser

--- src/main/kotlin/org/skellig/plugin/language/teststep/psi/SkelligTestStepTokenType.kt
+++ src/main/kotlin/org/skellig/plugin/language/teststep/psi/SkelligTestStepTokenType.kt
@@ -3,8 +3,4 @@ package org.skellig.plugin.language.teststep.psi
 import com.intellij.psi.tree.IElementType
 
 class SkelligTestStepTokenType(debugName: String?) : IElementType(debugName!!, SkelligTestStepLanguage.INSTANCE) {
-
-    override fun toString(): String {
-        return "SkelligTestStepTokenType." + super.toString()
-    }
 }
