de9d04298f504618796f81e669011da776577b41
shendor <shendor1988@gmail.com>
2024-01-21 19:46:01 +0000

    Added reference to feature file from test step

--- src/main/kotlin/org/skellig/plugin/language/teststep/SkelligTestStepPsiImplUtil.kt
+++ src/main/kotlin/org/skellig/plugin/language/teststep/SkelligTestStepPsiImplUtil.kt
@@ -3,10 +3,13 @@ package org.skellig.plugin.language.teststep
 import com.intellij.navigation.ItemPresentation
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiElementFactory
 import com.intellij.psi.PsiFileFactory
 import com.intellij.psi.PsiReference
 import org.skellig.plugin.language.SkelligFileIcons
 import org.skellig.plugin.language.teststep.psi.*
+import org.skellig.plugin.language.teststep.psi.impl.SkelligTestStepTestStepNameImpl
+import org.skellig.plugin.language.teststep.psi.reference.SkelligTestStepToFeatureReference
 import org.skellig.plugin.language.teststep.psi.reference.SkelligTestStepToStateReference
 import org.skellig.plugin.language.teststep.psi.reference.SkelligTestStepToValuesAndPropertiesReference
 import javax.swing.Icon
@@ -15,8 +18,7 @@ object SkelligTestStepPsiImplUtil {
 
     @JvmStatic
     fun getName(element: SkelligTestStepTestStepName): String {
-        val node = element.node.findChildByType(SkelligTestStepTypes.TEST_STEP_NAME)
-        return node?.let { node.text.substring(1, node.text.length - 1) } ?: ""
+        return if(element.text.length > 2) element.text.substring(1, element.text.length - 1) else ""
     }
 
     @JvmStatic
@@ -58,10 +60,49 @@ object SkelligTestStepPsiImplUtil {
     }
 
     @JvmStatic
+    fun getReference(element: SkelligTestStepTestStepName): PsiReference {
+        return SkelligTestStepToFeatureReference(element)
+    }
+
+    @JvmStatic
     fun getPresentation(element: SkelligTestStepKey): ItemPresentation {
         return object : ItemPresentation {
             override fun getPresentableText(): String? {
                 return element.text
+            }
+
+            override fun getLocationString(): String {
+                return element.containingFile.name
+            }
+
+            override fun getIcon(unused: Boolean): Icon {
+                return SkelligFileIcons.TEST_DATA_FILE
+            }
+        }
+    }
+
+    @JvmStatic
+    fun getPresentation(element: SkelligTestStepTestStepName): ItemPresentation {
+        return object : ItemPresentation {
+            override fun getPresentableText(): String {
+                return element.name
+            }
+
+            override fun getLocationString(): String {
+                return element.containingFile.name
+            }
+
+            override fun getIcon(unused: Boolean): Icon {
+                return SkelligFileIcons.TEST_DATA_FILE
+            }
+        }
+    }
+
+    @JvmStatic
+    fun getPresentation(element: SkelligTestStepTestStepNameExpression): ItemPresentation {
+        return object : ItemPresentation {
+            override fun getPresentableText(): String {
+                return element.testStepName.name
             }
 
             override fun getLocationString(): String {
